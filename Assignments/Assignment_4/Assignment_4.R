996167184da4629b59638d6122cab8ae00f38f21
Ernie Vilela <ernievc@gmail.com>
2020-09-20 18:30:19 -0600

    3rd commit

--- Assignments/Assignment_4/Assignment_4.R
+++ Assignments/Assignment_4/Assignment_4.R
@@ -1,40 +1,6 @@
-?read.table() #This brings up the help file
-df <-  read.csv("../../Data/landdata-states.csv") # why did I change to read.csv ???
-class(df) # what type of object is df?
-head(df) # shows the first 6 elements of an object (first 6 rows if you give it a data frame)
-class(df$State)
-State_updated <- as.factor(df$State)
-class(State_updated)
-
-
-df[1]
-
-
-
-
-
-dfi <- read.csv("../../Data/landdata-states.csv", stringsAsFactors = FALSE)
-class(dfi) # what type of object is df?
-head(dfi) # shows the first 6 elements of an object (first 6 rows if you give it a data frame)
-class(dfi$Home.Value)
-
-
-
-
-
-
-
-
-library(ggplot2)
-
-
-df <-  read.csv("../../Data/landdata-states.csv") # why did I change to read.csv ???
-summary (df$Home.Value) # what type of object is df?
-var <- names(df)[4] # shows the first 6 elements of an object (first 6 rows if you give it a data frame)
-class(var)
-
-plot (x=df$Year, y=df$Land.Value, (as.factor(col=df$region)))
-
-
-ggplot(df, aes(y=Land.Value, x=Year,col=region))+geom_point()
-ggplot(df, aes(y=Land.Value, x=Year,col=region, shape=region))+geom_point()
+#Assignment_4
+df <-  read.table("../../Data/ITS_mapping.csv", header = TRUE, sep = "\t")
+png (filename = "./silly_boxplot.png")
+plot (x=(as.factor(df$Ecosystem)), y = df$Lat, xlab = "Ecosystem", ylab = "Latitude", cex.lab=1, cex.axis=0.5, cex.main=1, cex.sub=1) 
+dev.off()
+?plot()
--- Assignments/Assignment_4/Assignment_4Extra.R
+++ Assignments/Assignment_4/Assignment_4Extra.R
@@ -1,13 +1,42 @@
-?read.csv()
+library(ggplot2)
 
-df <- read.csv("../../Data/landdata-states.csv", colClasses = c("factor","character","numeric","integer","integer","integer","numeric","numeric","numeric","integer","integer")) # why did I change to read.csv ???
-class(df) # what type of object is df?
-head(df) # shows the first 6 elements of an object (first 6 rows if you give it a data frame)
+#Questions 1, 2, 3
+# 4) turning into a factor instead of character
+?read.table() #This brings up the help file
+df <- read.csv("../../Data/landdata-states.csv", colClasses = c("factor")) # why did I change to read.csv ???
 class(df$State)
+class(df$region)
+class(df$Date)
+
+#or
+
+df <- read.csv("../../Data/landdata-states.csv", stringsAsFactors = TRUE)
+class(df$State)
+class(df$region)
+class(df$Date)
 
 
-State_updated <- as.factor(df$State)
-class(State_updated)
+#5. What command would give the summary stats for ONLY the Home.Value column?
+summary (df$Home.Value)
+#6) What happens when you run names (df)[4]
+names (df)[4]
+
+#7. What is happening when you add (…col=df$region) to the above plotting code?
+#In other words, what happens when you run: 
 
 
-?read.csv()
+
+df <-  read.table("../../Data/ITS_mapping.csv", header = TRUE, sep = "\t")
+
+
+df <-  read.csv("../../Data/landdata-states.csv") # why did I change to read.csv ???
+summary (df$Home.Value) # what type of object is df?
+var <- names(df)[4] # shows the first 6 elements of an object (first 6 rows if you give it a data frame)
+class(var)
+
+plot (x=df$Year, y=df$Land.Value, (as.factor(col=df$region)))
+ggplot(df, aes(y=Land.Value, x=Year,col=region))+geom_point()
+ggplot(df, aes(y=Land.Value, x=Year,col=region, shape=region))+geom_point()
+
+
+
